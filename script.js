76a16bb581b9ede341b2369df14fdd59ccd9e6e9
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-19 18:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var student = {
+    name: "John Doe"
+};
+
+// Add the getKeys() method to the Object prototype
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
+};
